916255067afb73015de45330bfa527b8e28db219
KsArT-IT <ksart.it@gmail.com>
2022-11-11 21:04:13 +0200

    init: start r&m task

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# RickAndMortyCompose
-Jetpack Compose, android-application, clean-architecture, usecase-pattern, repository-pattern, kotlin, kotlin-dsl, kotlin-coroutines, navigation, dagger-hilt, coil.
+# Rick and Morty Compose
+
+Display of characters from the animated series Rick and Morty
+
+<p>
+<img src="./img/title.png" width="100%" height="100%"/>
+</p>
+
+## Architecture
+
+- [MVVM](https://ru.wikipedia.org/wiki/Model-View-ViewModel)
+- [Clean-Architecture](https://blog.cleancoder.com/uncle-bob/2012/08/13/the-clean-architecture.html)
+- [Use Case](https://en.wikipedia.org/wiki/Use_case)
+
+## Technologies
+
+- [Jetpack Compose](https://developer.android.com/jetpack/compose)
+- [Kotlin](https://kotlinlang.org) - %100 Kotlin
+- [Gradle Kotlin DSL](https://docs.gradle.org/current/userguide/kotlin_dsl.html)
+- [Coroutines](https://github.com/Kotlin/kotlinx.coroutines) for asynchronous operations
+- [Lifecycle-ktx](https://developer.android.com/kotlin/ktx)
+- [ViewModel](https://developer.android.com/topic/libraries/architecture/viewmodel)
+- [Dagger Hilt](https://developer.android.com/training/dependency-injection/hilt-android) for Dependency Injection
+- [Retrofit](https://github.com/square/retrofit) for network operations
+- [OkHttp](https://github.com/square/okhttp)
+- [Coil](https://coil-kt.github.io/coil/compose/) for image loading
+- [Api](https://rickandmortyapi.com/documentation/#get-all-characters)
+
+
---
+++ app/build.gradle.kts
@@ -0,0 +1,114 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "pro.ksart.rickandmorty"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "pro.ksart.rickandmorty"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildToolsVersion = "33.0.0"
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Lifecycle KTX
+    val lifecycleVers = "2.5.1"
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVers")
+    // Saved State module for ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycleVers")
+    // Compose
+    val composeVersion = "1.3.2"
+    implementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    implementation("androidx.compose.compiler:compiler:$composeVersion")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    // Compose dependencies
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVers")
+    implementation("androidx.navigation:navigation-compose:$lifecycleVers")
+    implementation("com.google.accompanist:accompanist-flowlayout:0.27.0")
+    // SplashScreen on devices prior Android 12
+    implementation("androidx.core:core-splashscreen:1.0.0")
+    // Coroutines and Flow
+    val coroutinesVersion = "1.6.4"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+    // Dagger Hilt
+    val hiltVersion = "2.44"
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    // Retrofit
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
+    // Okhttp
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.3")
+    // Moshi
+    val moshiVersion = "1.14.0"
+    implementation("com.squareup.moshi:moshi:$moshiVersion")
+    implementation("com.squareup.moshi:moshi-kotlin:$moshiVersion")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
+    // Paging3
+    implementation("androidx.paging:paging-runtime-ktx:3.1.1")
+    // Paging3 - Jetpack Compose integration
+    implementation("androidx.paging:paging-compose:1.0.0-alpha17")
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.2.2")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
